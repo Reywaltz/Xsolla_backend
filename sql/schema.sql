f2d7ac844d840f9a88d580572d51bf78bf6748cf
Reywaltz <george1147@yandex.ru>
2021-07-03 01:06:13 +0300

    Updated sql scripts & docker-compose file

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,4 +3,12 @@ CREATE TABLE IF NOT EXISTS item (
     name text NOT NULL,
     type text NOT NULL,
     cost numeric
-)
+);
+
+INSERT INTO item (SKU, name, type, cost) VALUES ('DOT-SUB', 'Dota Plus', 'Subscription', '9.99');
+INSERT INTO item (SKU, name, type, cost) VALUES ('SON-GAM', 'Sonic rangers', 'Game', '59.99');
+INSERT INTO item (SKU, name, type, cost) VALUES ('DMC-GAM', 'DMC:Devil may cry', 'Game', '69.99');
+INSERT INTO item (SKU, name, type, cost) VALUES ('OVE-GAM', 'Overwatch', 'Game', '39.99');
+INSERT INTO item (SKU, name, type, cost) VALUES ('HOT-GAM', 'Hotline Miami', 'Game', '49.99');
+INSERT INTO item (SKU, name, type, cost) VALUES ('XBO-SUB', 'Xbox Gamepass', 'Subsription', '5.99');
+INSERT INTO item (SKU, name, type, cost) VALUES ('PLA-SUB', 'Playstation Plus', 'Subsription', '6.99');
